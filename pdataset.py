720b7b6050ccdcacf9772f7ddbaf267760ee64f3
TaiChunYen <kevintai0304@gmail.com>
2019-06-12 20:20:05 +0800

    yolo bbox to predict position

---
+++ pdataset.py
@@ -0,0 +1,42 @@
+from torch.utils.data.dataset import Dataset
+import os
+import json
+
+class customDataset(Dataset):
+    def __init__(self,root,lab_root,transform=None):
+        ##############################################
+        ### Initialize paths, transforms, and so on
+        ##############################################
+        self.root = root
+        self.lab_root = lab_root
+        self.transform = transform
+        self.yolo = []
+        self.label = []
+        self.len = 0
+
+        f = open(self.root, "r")
+        jf = json.loads(f.read())
+        f2 = open(self.lab_root, "r")
+        jf2 = json.loads(f2.read())
+        
+        for i in range(len(jf)):
+            if jf2[jf[i]['filename'][-13:-3]+'png'] != None:
+                self.yolo.append(jf[i]['output'][-4:])
+                self.label.append(jf2[jf[i]['filename'][-13:-3]+'png'][0])
+
+        self.len = len(self.yolo)    
+
+    def __getitem__(self, index):
+        ##############################################
+        # 1. Read from file (using numpy.fromfile, PIL.Image.open)
+        # 2. Preprocess the data (torchvision.Transform).
+        # 3. Return the data (e.g. image and label)
+        ##############################################
+        return self.yolo[index], self.label[index]
+
+
+    def __len__(self):
+        ##############################################
+        ### Indicate the total size of the dataset
+        ##############################################
+        return self.len
---
+++ pp_model.py
@@ -0,0 +1,16 @@
+import torch
+
+class PP(torch.nn.Module):
+
+    def __init__(self):
+        super(ELMo, self).__init__()
+        self.input = torch.nn.Linear(4,512)
+        self.pp = torch.nn.Linear(512,512)
+        self.output = torch.nn.Linear(512,3)
+
+    def forward(self, bbox):
+        out = self.input(bbox)
+        out2 = self.pp(out)
+        out3 = self.output(out2)
+
+        return out3
